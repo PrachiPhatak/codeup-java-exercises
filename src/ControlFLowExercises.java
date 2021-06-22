18c76de7fd210493a356cca9274ed032b50c78d3
PrachiPhatak <phatak.prachi@gmail.com>
2021-06-22 14:10:38 -0500

    Adds controls statements and loops exercise - b2

--- src/ControlFLowExercises.java
+++ src/ControlFLowExercises.java
@@ -1,9 +1,9 @@
 public class ControlFLowExercises {
     public static void main(String[] args) {
-        int i = 0;
+        int i = 100;
         do{
             System.out.println(i);
-            i+=2;
-        }while(i <= 100);
+            i-=2;
+        }while(i >= 0);
     }
 }
