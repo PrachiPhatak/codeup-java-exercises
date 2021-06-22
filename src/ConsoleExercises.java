464b2d6594717ffe9052324f2101c50ec56ac9c5
PrachiPhatak <phatak.prachi@gmail.com>
2021-06-22 10:34:13 -0500

    Adds java program exercise

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,14 @@
+public class ConsoleExercises {
+    public static void main(String[] args) {
+
+
+        System.out.print("My name is Casey");
+        System.out.println(" Edwards");
+
+        String name ="Prachi";
+
+        String mySentence = String.format("My name is %s and I am a %s.", name, "Student");
+        System.out.printf("I will sell my land for $%f", 100.006);
+        System.out.println(mySentence);
+    }
+}
