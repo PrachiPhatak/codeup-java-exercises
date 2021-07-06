eeccd2853ae94530bca125c994aaea130f70cab6
PrachiPhatak <phatak.prachi@gmail.com>
2021-07-06 12:25:40 -0500

    Adds classroom exercise

---
+++ src/company/ABCompany.java
@@ -0,0 +1,8 @@
+package company;
+
+public class CompanyEmployee {
+    public static void main(String[] args) {
+        Employee emp = new Manager();
+        System.out.println(emp.doesWork());
+    }
+}
