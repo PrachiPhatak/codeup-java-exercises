f2e8d3286ea6961454b165ff0f1d3b6328b681b9
PrachiPhatak <phatak.prachi@gmail.com>
2021-07-13 10:31:59 -0500

    Adds annotaion class exercises

---
+++ src/AnnotationMain.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnnotationMain {
+    public static void main(String[] args) {
+        @SuppressWarnings("unused")
+        int i = 0;
+
+    }
+
+    @SuppressWarnings("unused")
+    public List<String> languages(){
+        return new ArrayList<>();
+    }
+}
---
+++ src/CodeWarsSmiles.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CodeWarsSmiles {
+    public static void main(String[] args) {
+        List<String> strList = new ArrayList<>();
+    }
+}
