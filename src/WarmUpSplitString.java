c8f14da3340cf15c1744aa69fd85396079d01f17
PrachiPhatak <phatak.prachi@gmail.com>
2021-07-14 10:24:21 -0500

    Adds exception warmup exercise solution

--- src/WarmUpSplitString.java
+++ src/WarmUpSplitString.java
@@ -1,15 +1,35 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class WarmUpSplitString {
 
     public static void main(String[] args) {
 
-        for (String s : spitString("Prachi Phatak Katy Texas Maharashtra India")) {
+
+        printSplitString(spitString("Prachi Phatak Katy Texas Maharashtra India"));
+        try{
+            ArrayList<Integer> myArrList = new ArrayList<Integer>();
+            System.out.println(myArrList.get(2));
+
+        }catch (Exception e){
+            System.out.println("Not a number");
+        }
+
+
+        try {
+            int myNum = Integer.parseInt("eighteen");
+        } catch (Exception e) {
+            System.out.println("The input is not integer");
+        }
+    }
+
+    public static void printSplitString(String[] stringArr) {
+        for (String s : stringArr) {
             System.out.println(s);
         }
     }
 
-    public static String[] spitString(String string){
+    public static String[] spitString(String string) {
         return string.split(" ");
     }
 }
