9ae280eb9a5ab7b3c77dc428297db87dc161b83a
PrachiPhatak <phatak.prachi@gmail.com>
2021-07-15 16:56:54 -0500

    Adds I/O exercise

--- src/fileIO/FileIO.java
+++ src/fileIO/FileIO.java
@@ -4,6 +4,8 @@ import javax.imageio.IIOException;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileIO {
     public static void main(String[] args) {
@@ -19,7 +21,18 @@ public class FileIO {
         path = Paths.get(path.toString(), "test.txt");
         FileDirectoryUtil.tryCreateFile(path);
 
+
+
+        IOUtil.tryWriteToFile(path, getContent());
         IOUtil.tryPrintContent(path);
     }
 
+    public static List<String>  getContent(){
+        List<String> contentToWrite = new ArrayList<String>();
+        contentToWrite.add("This is Prachi Phatak");
+        contentToWrite.add("This is Prachi Phatak1");
+        contentToWrite.add("This is Prachi Phatak2");
+
+        return contentToWrite;
+    }
 }
--- src/fileIO/IOUtil.java
+++ src/fileIO/IOUtil.java
@@ -3,25 +3,34 @@ package fileIO;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
 
 public class IOUtil {
+
+    public static void tryPrintContent(Path path) {
+        try {
+            IOUtil.printFileContent(IOUtil.readFromFile(path));
+        } catch (IOException e) {
+            System.out.println("Unable to read contents of the file at : " + path);
+        }
+    }
 
     public static List<String> readFromFile(Path path) throws IOException {
         return Files.readAllLines(path);
     }
 
-    public static void printFileContent(List<String> content){
+    public static void printFileContent(List<String> content) {
         for (String line : content) {
             System.out.println(line);
         }
     }
 
-    public  static void tryPrintContent(Path path){
-        try {
-            IOUtil.printFileContent(IOUtil.readFromFile(path));
-        } catch (IOException e) {
-            System.out.println("Unable to read contents of the file at : " + path);
+    public static void tryWriteToFile(Path path, List<String> content) {
+        try{
+            Files.write(path, content, StandardOpenOption.APPEND);
+        }catch(IOException e){
+            System.out.println("Unable to write contents to the file at : " + path);
         }
     }
 }
